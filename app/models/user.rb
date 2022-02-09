a9730a8735db23520eb49aec27c6715115d540f6
keitogit <kate_m@me.com>
2022-02-10 00:12:03 +0900

    単体テストコードの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :nickname,               presence: true
+  validates :password,               presence: true, format: {with: /\A[a-z0-9]+\z/ }
+  validates :password_confirmation,  presence: true, length: { minimum: 6 }
+  validates :family_name,            presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name_kana,       presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name,             presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_kana,        presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_day, presence: true
 end
