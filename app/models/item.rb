eeec2dcf6e96d0f0df83c52e8ccdb189ddd3e684
keitogit <kate_m@me.com>
2022-02-10 15:14:28 +0900

    validationの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,14 @@
 class Item < ApplicationRecord
-  belong_to :user
   has_one :buying_history
   has_one_attached :image
+
+ validates :name,                presence: true
+ validates :price,               presence: true
+ validates :description,         presence: true
+ validates :atatus_id,           presence: true
+ validates :shipping_cost_id,    presence: true
+ validates :shipping_day_id,     presence: true
+ validates :prefecture_id,       presence: true
+ validates :category_id,         presence: true
+ validates :user,                presence: true
 end
