9fbc4bd8ce06c18f703be9d2a5a0e54a736fbf25
keitogit <kate_m@me.com>
2022-02-10 11:12:14 +0900

    修正、users_controllerの削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   get 'items/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "items#index"
-  resources :users, only: [:new, :create, :index]
 end
