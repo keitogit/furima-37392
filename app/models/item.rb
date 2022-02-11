b5ebf0ee250533756e975165cb92edfd18194ba0
keitogit <kate_m@me.com>
2022-02-11 11:51:15 +0900

    itemモデルテストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,29 @@
 class Item < ApplicationRecord
   has_one :buying_history
   has_one_attached :image
-  belongs_to :products
+  belongs_to :user
+
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :status
+  belongs_to :shipping_cost
+  belongs_to :shipping_day
 
   validates :name,                presence: true
-  validates :price,               presence: true
+  validates :price,               presence: true, numericality: { greater_than_or_equal_to: 300 }
   validates :description,         presence: true
   validates :status_id,           presence: true
   validates :shipping_cost_id,    presence: true
   validates :shipping_day_id,     presence: true
   validates :prefecture_id,       presence: true
-  validates :category_id,         presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id,         presence: true
   validates :user,                presence: true
+
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+    validates :prefecture_id
+    validates :status_id
+    validates :shipping_cost_id
+    validates :shipping_day_id
+  end
 end
