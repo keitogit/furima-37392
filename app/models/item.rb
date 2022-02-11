61f1accab77d11f6cb0f640fc8ef96a15269c3f1
keitogit <kate_m@me.com>
2022-02-11 10:26:46 +0900

    Activehashのモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,15 @@
 class Item < ApplicationRecord
   has_one :buying_history
   has_one_attached :image
+  belongs_to :products
 
- validates :name,                presence: true
- validates :price,               presence: true
- validates :description,         presence: true
- validates :atatus_id,           presence: true
- validates :shipping_cost_id,    presence: true
- validates :shipping_day_id,     presence: true
- validates :prefecture_id,       presence: true
- validates :category_id,         presence: true
- validates :user,                presence: true
+  validates :name,                presence: true
+  validates :price,               presence: true
+  validates :description,         presence: true
+  validates :status_id,           presence: true
+  validates :shipping_cost_id,    presence: true
+  validates :shipping_day_id,     presence: true
+  validates :prefecture_id,       presence: true
+  validates :category_id,         presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :user,                presence: true
 end
